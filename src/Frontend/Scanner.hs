7d8921ce6d2a7d85f9ad509c91a7e5638ccf0c4d
Syed Moiz Ur Rehman <syedmoizurrehman@hotmail.com>
2019-12-25 22:21:18 +0500

    Include license header in each source file.

--- src/Frontend/Scanner.hs
+++ src/Frontend/Scanner.hs
@@ -1,3 +1,22 @@
+{-
+Copyright (C) 2019  Syed Moiz Ur Rehman
+
+This file is part of Embers.
+
+Embers is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+Embers is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Embers.  If not, see <https://www.gnu.org/licenses/>.
+-}
+
 module Frontend.Scanner
 where
 
