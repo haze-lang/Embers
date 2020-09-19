a04f7bb9bddf00c1594b8504585f1088a832a6a2
Syed Moiz Ur Rehman <syedmoizurrehman@hotmail.com>
2020-09-19 20:32:17 +0500

    Compiler Args: Ground work for emitting intermediate representations.

--- app/Args.hs
+++ app/Args.hs
@@ -3,15 +3,29 @@ module Args where
 import Options.Applicative
 import Data.Semigroup ((<>))
 
-data Args = Args { optInput :: Input }
+data Args = Args {
+    fileInput :: Input,
+    genIR :: Bool,
+    genSimplified :: Bool }
 
 newtype Input = FileInput FilePath
 
 input :: Parser Input
-input = (FileInput <$> strOption
-    (   short 'f'
-    <>  metavar "FILENAME"
-    <>  help "Input file" ))
+input = FileInput
+    <$> strOption
+        (   short 'f'
+        <>  metavar "FILENAME"
+        <>  help "Input file" )
+
+emitIR :: Parser Bool
+emitIR = switch
+          ( short 'i'
+         <> help "Whether to emit intermediate code" )
+
+emitSimpleAST :: Parser Bool
+emitSimpleAST = switch
+          ( short 'c'
+         <> help "Whether to emit simplified AST" )
 
 parseArgs :: Parser Args
-parseArgs = Args <$> (helper <*> input)
+parseArgs = Args <$> input <*> emitIR <*> emitSimpleAST <**> helper
